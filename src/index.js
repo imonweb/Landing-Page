cb9457e23849365258868a9a1476297e0df40479
imonweb <contact@imonweb.co.uk>
2021-05-19 12:26:45 +0100

    media queries

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import '../src/css/styles.scss';
+
+window.onload = function(){
+   window.addEventListener('scroll', (e) => {
+    if(window.pageYOffset > 100){
+      document.querySelector("header").classList.add('is-scrolling')
+    } else {
+      document.querySelector("header").classList.remove('is-scrolling')
+    }
+   })
+}
